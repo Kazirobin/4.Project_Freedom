7aee2fccdf6a8480c9bdff7925e5b27db2c242c8
Kazi Robin <kazirobinking@gmail.com>
2023-12-25 07:06:59 +0600

    up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
-// navbar bar 
-const bar = document.querySelector('.navbar__bars');
-const menu = document.querySelector('.navbar__menu');
-bar.addEventListener('click',() => {
-    menu.classList.toggle('active')
-})
+// navbar bar
+const bar = document.querySelector(".navbar__bars");
+const menu = document.querySelector(".navbar__menu");
+bar.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+// JavaScript to toggle visibility of sections
+function showSection(sectionId) {
+  // Toggle visibility of all sections except 'all'
+  document.querySelectorAll("section:not(#all)").forEach((section) => {
+    section.style.display =
+      section.id === sectionId || sectionId === "all" ? "block" : "none";
+  });
+}
